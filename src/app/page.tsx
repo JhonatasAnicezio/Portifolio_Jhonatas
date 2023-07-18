3ba885603e6c136e3ea43c516b15c0a6d8f97309
Jhonatas Anicezio <jhonatasanic@hotmail.com>
2023-07-17 22:01:40 -0300

    feat: add responsive for mobile

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -5,12 +5,13 @@ export default function Home() {
   return (
     <main className="w-full">
       <div className="flex flex-col h-screen justify-center items-center w-full
-        2xl:gap-5 xl:gap-5 lg:gap-4 md:gap-3 sm:gap-2
+        2xl:gap-5 xl:gap-5 lg:gap-4 md:gap-3 sm:gap-2 max-sm:gap-5
       ">
         <NavPages />
-        <div className="flex flex-col items-center w-3/5 border-y border-gray-300
-            lg:p-9 sm:p-7
-            2xl:gap-8 xl:gap-6 lg:gap-5 md:gap-3 sm:gap-3
+        <div className="flex flex-col items-center border-y border-gray-300
+            sm:w-3/5 max-sm:w-3/4 
+            lg:p-9 sm:p-7 max-sm:p-6
+            2xl:gap-8 xl:gap-6 lg:gap-5 md:gap-3 sm:gap-3 max-sm:gap-6
           "
         >
           <img src="./person.jpg" className="rounded-full
@@ -19,20 +20,20 @@ export default function Home() {
             "
           />
           <h1 className="text-gray-700 font-bold
-              2xl:text-5xl xl:text-4xl lg:text-3xl md:text-2xl sm:text-xl text-4xl
+              2xl:text-5xl xl:text-4xl lg:text-3xl md:text-2xl sm:text-xl max-sm:text-2xl
             "
           >
             Jhonatas Anicezio
           </h1>
           <h2 className="text-gray-700
-              2xl:text-2xl xl:text-xl lg:text-lg md:text-sm text-sm
+              2xl:text-2xl xl:text-xl lg:text-lg sm:text-sm max-sm:text-base
             "
           >
             Full Stack Developer
           </h2>
           <div className="border-t border-gray-300 w-[10%]" />
           <h2 className="text-center text-gray-700
-              2xl:text-xl xl:text-base lg:text-xs md:text-[10px] sm:text-[8px] text-xs
+              2xl:text-xl xl:text-base lg:text-xs md:text-[10px] sm:text-[8px] max-sm:text-xs
             "
           >
             Tenho 20 anos e atualmente trabalho com desenvolvimento de aplicações front-end em Next.js, e Node.js na parte do back, com 1 de experiencia com trabalho freelancer
--- src/components/Nav/Link.tsx
+++ src/components/Nav/Link.tsx
@@ -10,8 +10,8 @@ export function LinkNav(props: LinkProps) {
   return (
     <Link
       className="border hover:bg-gray-400 hover:border-gray-400 hover:text-white border-gray-300 duration-500 rounded-full
-        lg:py-1.5 md:py-1 sm:py-1
-        lg:px-5 md:px-4 sm:px-4
+        lg:py-1.5 md:py-1 sm:py-1 max-sm:py-1
+        lg:px-5 md:px-4 sm:px-4 max-sm:px-4
         2xl:text-lg xl:text-base lg:text-sm md:text-[10px] sm:text-[8px]
       "
       {...props}
